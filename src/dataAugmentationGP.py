31110ab7d438aec689b1e0ca4805e90591ba0e03
Martin Klapacz <klapacz.martin@gmail.com>
2020-12-28 14:10:57 +0100

    reworked input acquisition

--- src/dataAugmentationGP.py
+++ src/dataAugmentationGP.py
@@ -32,6 +32,7 @@ class DataAugmentationGP(AbstractGP):
         self.lf_hf_adapt_ratio = lf_hf_adapt_ratio
         self.a = self.b = None
         self.augm_iterator = EvenAugmentation(self.n)
+        self.acquired_X = []
 
         lf_model_params_are_valid = (f_low is not None) ^ (
             (lf_X is not None) and (lf_Y is not None) and (lf_hf_adapt_ratio is not None))
@@ -71,13 +72,16 @@ class DataAugmentationGP(AbstractGP):
 
     def adapt(self, plot=False, X_test=None, Y_test=None, verbose=False):
         if plot == 'uncertainty':
-            self.__adapt_plot_uncertainties(X_test=X_test, Y_test=Y_test, verbose=verbose)
+            self.__adapt_plot_uncertainties(
+                X_test=X_test, Y_test=Y_test, verbose=verbose)
         elif plot == 'mean':
-            self.__adapt_plot_means(X_test=X_test, Y_test=Y_test, verbose=verbose)
+            self.__adapt_plot_means(
+                X_test=X_test, Y_test=Y_test, verbose=verbose)
         elif plot == False:
             self.__adapt_no_plot(verbose=verbose)
         else:
-            raise Exception('invalid plot mode, use mean, uncertainty or False')
+            raise Exception(
+                'invalid plot mode, use mean, uncertainty or False')
 
     def __adapt_plot_uncertainties(self, X_test=None, Y_test=None, verbose=False):
         X = np.linspace(self.a, self.b, 200).reshape(-1, 1)
@@ -86,7 +90,7 @@ class DataAugmentationGP(AbstractGP):
         subplots_per_row = int(np.ceil(np.sqrt(self.adapt_steps)))
         subplots_per_column = int(np.ceil(self.adapt_steps / subplots_per_row))
         fig, axs = plt.subplots(
-            subplots_per_row, 
+            subplots_per_row,
             subplots_per_column,
             sharey='row',
             sharex=True,
@@ -99,7 +103,6 @@ class DataAugmentationGP(AbstractGP):
             acquired_x = self.get_input_with_highest_uncertainty()
             if verbose:
                 print('new x acquired: {}'.format(acquired_x))
-            # add subplott in 
             _, uncertainties = self.predict(X)
             ax = axs.flatten()[i]
             ax.axes.xaxis.set_visible(False)
@@ -135,11 +138,29 @@ class DataAugmentationGP(AbstractGP):
                 print('new x acquired: {}'.format(acquired_x))
             self.fit(np.append(self.hf_X, acquired_x))
 
-    def get_input_with_highest_uncertainty(self, precision: int = 200):
-        X = np.linspace(self.a, self.b, precision).reshape(-1, 1)
-        _, uncertainties = self.predict(X)
-        index_with_highest_uncertainty = np.argmax(uncertainties)
-        return X[index_with_highest_uncertainty]
+    def get_input_with_highest_uncertainty(self, precision: int = 300):
+        if len(self.acquired_X) > 0:
+            uncertainty_intervals = np.concatenate([
+                np.array([self.a])[:, None],
+                self.acquired_X,
+                np.array([self.b])[:, None]
+            ])
+        else:
+            uncertainty_intervals = np.array([self.a, self.b])[:, None]
+        current_input = -1
+        current_uncertainty = -1
+        for i, _ in enumerate(uncertainty_intervals[:-1]):
+            X = np.linspace(
+                uncertainty_intervals[i], uncertainty_intervals[i+1], precision).reshape(-1, 1)
+            _, uncertainties = self.predict(X)
+            high_uncertainty_index = np.argmax(uncertainties)
+            if current_uncertainty < uncertainties[high_uncertainty_index]:
+                current_uncertainty = uncertainties[high_uncertainty_index]
+                current_input = X[high_uncertainty_index]
+                print(current_uncertainty)
+        self.acquired_X.append(current_input)
+        print('------')
+        return current_input
 
     def __adapt_lf(self):
         X = np.linspace(self.a, self.b, 100).reshape(-1, 1)
