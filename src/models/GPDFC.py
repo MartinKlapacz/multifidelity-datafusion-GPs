0a02e4a830a3b1e9c18c926365b751dca82c8bf7
Martin Klapacz <klapacz.martin@gmail.com>
2021-02-20 23:14:59 +0100

    plot_lengthscale method

--- src/models/GPDFC.py
+++ src/models/GPDFC.py
@@ -20,3 +20,15 @@ class GPDFC(MultifidelityDataFusion):
         super().__init__(name=name, input_dim=input_dim, num_derivatives=num_derivatives, tau=tau, f_exact=f_exact,
                          lower_bound=lower_bound, upper_bound=upper_bound, f_low=f_low, lf_X=lf_X, lf_Y=lf_Y,
                          lf_hf_adapt_ratio=lf_hf_adapt_ratio, use_composite_kernel=True, eps=eps, add_noise=add_noise)
+
+
+    def plot_lengthscale_hyperparams(self):
+        kern = self.kernel.to_dict()
+        lengthscale1, l1 = kern.get("parts").get(1).get("lengthscale")[0], 'l1'
+        lengthscale2, l2 = kern.get("parts").get(0).get("parts").get(0).get("lengthscale")[0], 'l2'
+        lengthscale3, l3 = kern.get("parts").get(0).get("parts").get(1).get("lengthscale")[0], 'l3'
+        l = [l1, l2, l3]
+        x = [i for i, _ in enumerate(l)]
+        plt.bar(x, [lengthscale1, lengthscale2, lengthscale3])
+        plt.xticks(x, l)
+        return lengthscale1, lengthscale2, lengthscale3
