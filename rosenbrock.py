b4f11d735d79c02088898b39047026b0e0fc4197
Martin Klapacz <klapacz.martin@gmail.com>
2021-03-04 14:19:46 +0100

    cleaned up rosenbrock

--- rosenbrock.py
+++ rosenbrock.py
@@ -13,7 +13,12 @@ tau = .01
 
 _, _, _, f_exact, f_low, X_test, Y_test = rosenbrock(20, 80, dim)
 
-# Make data.
+nargp = NARGP(
+    name='NARGP',
+    input_dim=dim,
+    f_exact=f_exact,
+    f_low=f_low,
+)
 
 gpdf2 = GPDF(
     name='GPDF2',
@@ -51,8 +56,7 @@ gpdfc4 = GPDFC(
     f_low=f_low,
 )
 
-# models = [gpdf2, gpdfc2, gpdf4, gpdfc4]
-models = [gpdfc2 ]#, gpdfc2, gpdf4, gpdfc4]
+models = [gpdf2, gpdfc2, gpdf4, gpdfc4]
 
 labels = ['gpdf2', 'gpdfc2', 'gpdf4', 'gpdfc4']
 sizes = [5, 10, 15, 20, 25]
@@ -63,36 +67,16 @@ mses = np.zeros((len(models), len(sizes)))
 for i, model in enumerate(models):
     for j, size in enumerate(sizes):
         mse_runs = []
+        # make average runs
         for _ in range(nruns):
-
-            X_train_hf, _, Y_train_lf, _, _, _, _ = rosenbrock(size, 80, dim)
-
-            # fig = plt.figure()
-            # ax = fig.gca(projection='3d')
-            # X1 = np.arange(-0, 1, .05)
-            # X2 = np.arange(-0, 1, .05)
-            # X1, X2 = np.meshgrid(X1, X2)
-
-            # X = np.vstack((X1.flatten(), X2.flatten())).T
-            # Y = f_exact(X)
-            # ax.scatter(X1, X2, Y)
-            # plt.show()
-
-            print(f_exact(X_train_hf))
-
+            X_train_hf, _, _, _, _, _, _ = rosenbrock(size, 80, dim)
             model.fit(X_train_hf)
-            model.plot()
-            preds = model.predict(X_test)
-            plt.scatter(X_train_hf[:,0], X_train_hf[:,1], f_exact(X_train_hf), c='black')
-            plt.scatter(X_test[:,0], X_test[:,1], f_exact(X_test), c='purple')
-            plt.show()
             new_mse = model.get_mse(X_test, Y_test)
             mse_runs.append(new_mse)
         
         mses[i, j] = np.array(mse_runs).mean()
         print(size, model, mse_runs)
 
-print(mses)
 
 for i in range(len(mses)):
     plt.plot(sizes, mses[i,:], '>-', label=labels[i])
